3058fa6c4b882b3bb24ab6b10344b88558b67c3a
compean <javier.compeanr@gmail.com>
2022-01-26 20:04:10 -0600

    login route done

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -61,7 +61,7 @@ module.exports = (sequelize, DataTypes) => {
   };
 
   User.prototype.validatePassword = function (password) {
-    return bcrypt.compareSync(password.this.hashedPassword.toString());
+    return bcrypt.compareSync(password, this.hashedPassword.toString());
   };
 
   User.getCurrentUserById = async function (id) {
