f041b27bea64c882e5b8e774c468a23b002b27bf
compean <javier.compeanr@gmail.com>
2022-02-01 20:02:56 -0600

    working on submit form page styles and component

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import "./LoginForm.css";
+
+const LoginFormPage = () => {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  if (sessionUser) return <Redirect to="/" />;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password })).catch(
+      async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      }
+    );
+  };
+
+  return (
+    <div className="container">
+      <div className="screen">
+        <div className="screen__content">
+          <form onSubmit={handleSubmit} className="login">
+            <ul>
+              {errors.map((error, idx) => (
+                <li key={idx}>{error}</li>
+              ))}
+            </ul>
+            <div className="login__field">
+              <i class="login__icon fas fa-user"></i>
+              <input
+                className="login__input"
+                type="text"
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                placeholder="User / Email"
+                required
+              />
+            </div>
+            <div className="login__field">
+              <i class="login__icon fas fa-lock"></i>
+              <input
+                className="login__input"
+                type="text"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+              />
+            </div>
+            <button type="submit" className="button login__submit">
+              <span className="button__text">Log In Now</span>
+            </button>
+          </form>
+        </div>
+        <div className="screen__background">
+          <span className="screen__background__shape screen__background__shape4"></span>
+          <span className="screen__background__shape screen__background__shape3"></span>
+          <span className="screen__background__shape screen__background__shape2"></span>
+          <span className="screen__background__shape screen__background__shape1"></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginFormPage;
