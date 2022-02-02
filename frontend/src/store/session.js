ebbf431b01ee2629e491788ea194b863ea3da5bd
compean <javier.compeanr@gmail.com>
2022-02-01 18:52:49 -0600

    session reducer

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -30,3 +30,23 @@ export const login = (user) => async (dispatch) => {
   dispatch(setUser(data.user));
   return response;
 };
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case SET_USER:
+      newState = Object.assign({}, state);
+      newState.user = action.payload;
+      return newState;
+    case REMOVE_USER:
+      newState = Object.assign({}, state);
+      newState.user = null;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
